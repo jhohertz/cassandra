1b59610ab55d6da980c3098207fa4a36b4d5af12
Joe Hohertz <joe@viafoura.com>
2014-07-30 15:39:40 -0400

    expose auto_bootstrap as a system property override

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -1133,7 +1133,7 @@ public class DatabaseDescriptor
 
     public static boolean isAutoBootstrap()
     {
-        return conf.auto_bootstrap;
+        return Boolean.parseBoolean(System.getProperty("cassandra.auto_bootstrap", conf.auto_bootstrap.toString()));
     }
 
     public static void setHintedHandoffEnabled(boolean hintedHandoffEnabled)
